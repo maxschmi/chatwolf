fed701b5736d4c979c686e9d598f191f094d5a57
Max Schmit <Max.Schmit@bwedu.de>
2020-05-12 04:01:03 +0200

    fix conf

--- chatwolf/_conf.py
+++ chatwolf/_conf.py
@@ -33,6 +33,6 @@ from os import path
 from pkg_resources import resource_filename as res_file
 
 #import config
-conf_json = open(res_file(__name__, "/data/conf.json"), "r")
+conf_json = open(os.path.realpath(__name__) + "/data/conf.json"), "r")
 _conf = json.load(conf_json)
 conf_json.close()
